665fdef2912ba6b77ead0cda41771f062b265e96
Matthew Higgins <mthiggins1s@semo.edu>
2025-02-04 11:35:51 -0600

    finalized Amazon Project

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,6 +81,13 @@ button.addEventListener('click', () => {
            });
     }
 
-   console.log(cart);
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
 });
 });
